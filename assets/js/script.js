5e5e1cce01c51b5b607be3f83008504042a0b3d8
cmwschroeder <cmwschroeder@gmail.com>
2022-04-06 00:43:35 -0700

    added first hour block to page

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+var allTImes = $(".container");
+
+function buildDay() {
+    var div = $('<div>');
+    div.addClass('time-block');
+    div.addClass('row');
+    div.addClass('col-sm-12');
+
+    var p = $('<p>');
+    p.addClass('hour');
+    p.text("9 a.m.");
+    p.addClass('col-sm-1');
+
+    var form = $('<form>');
+    var input = $('<textarea>');
+    input.addClass('present w-100 h-100');
+    form.append(input);
+    form.addClass('col-sm-10 p-0');
+
+    var btn = $('<button>');
+    btn.text('🔒');
+    btn.addClass('saveBtn');
+    btn.addClass('col-sm-1');
+
+    div.append(p);
+    div.append(form);
+    div.append(btn);
+    allTImes.append(div);
+};
+
+buildDay();
