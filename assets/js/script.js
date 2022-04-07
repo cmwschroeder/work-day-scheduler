86815f33de22eab684e04a38ff373178e773a5a3
cmwschroeder <cmwschroeder@gmail.com>
2022-04-06 21:55:41 -0700

    added saving and loading dates from local storage, displaying in time frames. Fixed logic for displaying current, past, and present hours

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,84 @@
+// use moment.js to get todays date
 var today = moment();
+//references to html elements on the page
 var allTimes = $(".container");
 var dateEl = $("#currentDay");
 
+//used to color code the hours depending on past, present, or if they are in the future.
 var currHour = today.hour();
+var currWorkDay;
 var isAm;
+var daySchedule = [
+    {
+        time: 9,
+        message: ""
+    },
+    {
+        time: 10,
+        message: ""
+    },
+    {
+        time: 11,
+        message: ""
+    },
+    {
+        time: 12,
+        message: ""
+    },
+    {
+        time: 1,
+        message: ""
+    },
+    {
+        time: 2,
+        message: ""
+    },
+    {
+        time: 3,
+        message: ""
+    },
+    {
+        time: 4,
+        message: ""
+    },
+    {
+        time: 5,
+        message: ""
+    }
+];
 
+/*
+ * The function that runs on a page load, builds calendar part and populates the text areas
+ */
 function buildDay() {   
+    //checks for the current hour of the day, makes it so the hour will be between 1-12 and saves wether it is in the pm or am
     if(currHour >= 12) {
-        am = false;
+        isAm = false;
         if (currHour > 12) {
             currHour = currHour - 12;
         }
     }
     else {
-        am = true;
+        isAm = true;
     }
     allTimes.addClass('mb-3');
-    console.log(today.hour());
-    for(var i = 9; i < 13; i++) {
+    //runs through all the hours needed to build in the day
+    for(var i = 9; i < 12; i++) {
         buildHour(i, true);
     }
+    buildHour(12, false);
     for(i = 1; i < 6; i++) {
         buildHour(i, false);
+    }
+
+    dateEl.text(today.format("MMM Do, YYYY"));
+
+    currWorkDay = JSON.parse(localStorage.getItem('schedule'));
+    if(currWorkDay == null) {
+        currWorkDay = daySchedule;
+    }
+    for(i = 0; i < allTimes.children().length; i++) {
+        allTimes.children().eq(i).children().eq(1).children().first().val(currWorkDay[i].message);
     }
 };
 
@@ -83,6 +140,7 @@ function buildHour(hour, am) {
     else {
         input.addClass('past');
     }
+    input.attr("id", "for-btn-" + hour);
     form.append(input);
     form.addClass('col-sm-10 p-0');
 
@@ -90,11 +148,25 @@ function buildHour(hour, am) {
     btn.text('🔒');
     btn.addClass('saveBtn');
     btn.addClass('col-sm-1');
+    btn.attr("data-number", hour);
 
     div.append(p);
     div.append(form);
     div.append(btn);
     allTimes.append(div);
 };
+
+function saveText(event) {
+    var btnClicked = event.currentTarget.getAttribute("data-number");
+    var input = $(event.currentTarget).siblings().eq(1).children().first().val();
+    for(var i = 0; i < currWorkDay.length; i++) {
+        if(currWorkDay[i].time == btnClicked) {
+            currWorkDay[i].message = input;
+        }
+    }
+    localStorage.setItem("schedule", JSON.stringify(currWorkDay));
+}
+
+allTimes.on("click", ".saveBtn", saveText);
 
 buildDay();
